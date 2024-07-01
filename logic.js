db7ac64379f2dd05f74ac8bb882926a9943a3178
EragonBrisingr <geometrydasher26@gmail.com>
2024-07-01 14:27:56 +0300

    first commit

---
+++ logic.js
@@ -0,0 +1,26 @@
+
+
+
+function syntaxChange(){
+    const input = document.getElementById("user-input").innerHTML;
+    const output = document.getElementById("output");
+
+    let newHtml;
+
+    let open = false;
+
+    console.log(input[5]);
+
+    for(let i = 0; i<input.length;i++){
+
+        
+
+        if(input[i] == "+" && input[i+1] == "+"){
+
+        }
+
+    }
+
+
+
+}
