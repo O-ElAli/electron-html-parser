d1cf7a59eda0ad6c955b6fb69fa6020d5e55dad0
EragonBrisingr <geometrydasher26@gmail.com>
2024-07-02 14:35:51 +0300

    tags are chosen by the user instead. added bold and italic options

--- logic.js
+++ logic.js
@@ -1,5 +1,20 @@
 
 
+var paragraph = "";
+var bold = "";
+var italic = "";
+
+function parsing(){
+
+    paragraph = document.getElementById("paragraph").value;
+    bold = document.getElementById("bold").value;
+    italic = document.getElementById("italic").value;
+
+    console.log(paragraph)
+
+}
+
+console.log(paragraph)
 
 function syntaxChange(){
     const input = document.getElementById("user-input").value;
@@ -9,11 +24,11 @@ function syntaxChange(){
 
     let openP = false;
     let openB = false;
-    let openU = false;
+    let openI = false;
 
     for(let i = 0; i<input.length;i++){
 
-        if(input[i] == "+" && input[i+1] == "+"){
+        if(input[i] == paragraph[0] && input[i+1] == paragraph[1]){
 
             if(!openP){
                 //if open is false, open a p tag, change the value to true and increment to skip the second "+"
@@ -29,7 +44,7 @@ function syntaxChange(){
             }
         }
 
-        else if(input[i] == "*" && input[i+1] == "*"){
+        else if(input[i] == bold[0] && input[i+1] == bold[1]){
             if(!openB){
                 //if open is false, open a p tag, change the value to true and increment to skip the second "*"
                 newHtml+="<strong>";
@@ -40,6 +55,21 @@ function syntaxChange(){
                 //if open is true, close a p tag, change the value to false and increment to skip the second "*"
                 newHtml+="</strong>";
                 openB = false;
+                i++
+            }
+        }
+
+        else if(input[i] == italic[0] && input[i+1] == italic[1]){
+            if(!openI){
+                //if open is false, open a p tag, change the value to true and increment to skip the second "*"
+                newHtml+="<i>";
+                openI = true;
+                i++;
+            }
+            else{
+                //if open is true, close a p tag, change the value to false and increment to skip the second "*"
+                newHtml+="</i>";
+                openI = false;
                 i++
             }
         }
